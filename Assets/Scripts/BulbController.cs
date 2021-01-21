a64f4d1cb86df406400ff065658a605e3d900868
Toby.Taylor <toby.taylor20@yahoo.com>
2021-01-21 10:28:38 +0000

    Initial commit

---
+++ Assets/Scripts/BulbController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulbController : MonoBehaviour
+{
+    private bool bulbOff = true;
+
+    private Animator bulbAnimator;
+
+
+
+    void Awake()
+    {
+        bulbAnimator = gameObject.GetComponent<Animator>();
+
+        turnOff();
+    }
+
+    public void turnOff()
+    {
+        bulbOff = true;
+        bulbAnimator.SetBool("BulbOff", bulbOff);
+    }
+
+    public void turnOn()
+    {
+        bulbOff = true;
+        bulbAnimator.SetBool("BulbOn", bulbOff);
+    }
+
+}
